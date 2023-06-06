55ef9b2eb4e5be52e94d500b1bc8b2410e52e168
chukusuccess <successchuku5@gmail.com>
2023-06-07 00:26:07 +0100

    fixed url constructor replace bug

--- appWrite-client/settings.config.js
+++ appWrite-client/settings.config.js
@@ -1,7 +1,9 @@
 import { Client, Databases } from "appwrite";
 
+const id = process.env.NEXT_PUBLIC_PROJECT_ID;
+
 export const client = new Client()
-  .setEndpoint(process.env.NEXT_PUBLIC_CLIENT_URL)
-  .setProject(process.env.NEXT_PUBLIC_PROJECT_ID);
+  .setEndpoint("https://cloud.appwrite.io/v1")
+  .setProject(id);
 
 export const databaseClient = new Databases(client);
